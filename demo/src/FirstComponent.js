402796454315203d925814b85292aca4345d5c12
Steph Segura <steph_segura@Stephs-MacBook-Pro.local>
2021-05-25 16:24:04 -0600

    mgmt banner and gradient

--- demo/src/FirstComponent.js
+++ demo/src/FirstComponent.js
@@ -1,9 +1,11 @@
 import React from "react";
+import mgmt from "../../assets/MGMT.svg";
+
 
 export default () => {
   return (
-    <div className="component first-component">
-      <h2>First Component</h2>
+    <div  className="component first-component" >
+      <img src={mgmt} height={900} width={900}  />
     </div>
   );
 };
