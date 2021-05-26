3a2919c2edbcaa2a76798c84cdac4a1ece7b28e3
Steph Segura <steph_segura@Stephs-MacBook-Pro.local>
2021-05-25 19:32:14 -0600

    mgmt banner

--- demo/src/FirstComponent.js
+++ demo/src/FirstComponent.js
@@ -1,5 +1,7 @@
 import React from "react";
 import mgmt from "../../assets/MGMT.svg";
+import album from "../../assets/Album.png";
+
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import AudioPlayer from "react-h5-audio-player";
 import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
@@ -8,8 +10,11 @@ import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
 export default () => {
   return (
     <div className="component first-component">
-      <img src={mgmt} style={{ fontColor: "red" }} height={1000} width={1000} />
-      <div></div>
+      <img src={mgmt} style={{ fontColor: "red" }} height={775} width={775} />
+      <div className="banner">
+      <img src={album} style={{ fontColor: "red" }}  height={200} width={200} style={{padding: 20}}/>
+
+      </div>
     </div>
   );
 };
