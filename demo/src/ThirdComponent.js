7be3ae75d98786f7e6dda46fb40fc9afc5a3eca3
Steph Segura <steph_segura@Stephs-MacBook-Pro.local>
2021-05-27 19:58:35 -0600

    indents

--- demo/src/ThirdComponent.js
+++ demo/src/ThirdComponent.js
@@ -3,19 +3,27 @@ import React from "react";
 export default () => {
   return (
     <div className="component third-component">
-      <div style={{ height: "20%", backgroundColor: "black", color: "white",     display: "flex",
-    alignItems: "center",
-    justifyContent: "center", }}>
+      <div
+        style={{
+          height: "20%",
+          backgroundColor: "black",
+          color: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <h1 className="fontfam">Videos</h1>
       </div>
-        <iframe
-          width="100%" height="100%"
-          src="https://www.youtube.com/embed/videoseries?list=PLb9UY1Wp8pyY-tlMc1-EV-qJnP31T8_m0"
-          playlist="1"
-          frameborder="0"
-          allow="autoplay; encrypted-media"
-          allowfullscreen
-        ></iframe>
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/videoseries?list=PLb9UY1Wp8pyY-tlMc1-EV-qJnP31T8_m0"
+        playlist="1"
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 };
